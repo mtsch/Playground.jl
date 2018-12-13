1cfa23f6535dc4f11e31b1e60e0b53df1cd2cf36
mtsch <matijacufar@gmail.com>
2018-12-13 11:29:09 +0100

    complex can now take landmarks as arguments, rips complex removed

--- src/geodesiccomplex.jl
+++ src/geodesiccomplex.jl
@@ -11,35 +11,29 @@ using StaticArrays
 using Random
 using SparseArrays
 
-struct GeodesicComplex{T, D, P<:SVector{D, T}, M<:Metric, K<:NNTree{P, M}} <:
+struct GeodesicComplex{T, P<:AbstractVector{T}, M<:Metric, K<:NNTree{P, M}} <:
                        AbstractSimpleWeightedGraph{Int, T}
-    points    ::Vector{P}
-    landmarks ::Vector{Int}
-    graph     ::SimpleWeightedGraph{Int, T}
-    tree      ::K # is this needed?
-    cover     ::Vector{Set{Int}}
-    radius    ::T
-    metric    ::M
-    witness   ::Bool
+    points        ::Vector{P}
+    landmark_idxs ::Vector{Int}
+    graph         ::SimpleWeightedGraph{Int, T}
+    tree          ::K
+    cover         ::Vector{Set{Int}}
+    radius        ::T
+    metric        ::M
 end
 
-# TODO:
-# Mogoče je treba r ---> 2r, ker drugače lahko dobiš trikotnike, ki ne morejo imet
-# presečišča, ne glede na gostoto točk. Je 2r vedno dovolj?
-function GeodesicComplex(pts::AbstractVector{SVector{D, T}}, r;
-                         metric = Euclidean(), tree = KDTree,
-                         witness = true) where {D, T}
+function GeodesicComplex(pts::AbstractVector{<:AbstractVector{T}}, r; landmarks = nothing,
+                         metric = Euclidean(), tree = KDTree) where {T}
     kdt = tree(pts, metric)
-    landmarks, cover = getcover(pts, r, kdt)
-    n = length(landmarks)
-
+    landmark_idxs, cover = getcover(landmarks, pts, r, kdt)
     I = Int[]
     J = Int[]
     V = T[]
+    n = length(landmark_idxs)
+
     for i in 1:n, j in 1:(i - 1)
-        d = evaluate(metric, pts[landmarks[i]], pts[landmarks[j]])
-        if (witness && !isempty(intersect(cover[i], cover[j]))) ||
-           (!witness && d < 2r)
+        if !isempty(intersect(cover[i], cover[j]))
+            d = evaluate(metric, pts[landmark_idxs[i]], pts[landmark_idxs[j]])
             append!(I, (i, j))
             append!(J, (j, i))
             append!(V, (d, d))
@@ -47,53 +41,77 @@ function GeodesicComplex(pts::AbstractVector{SVector{D, T}}, r;
     end
     graph = SimpleWeightedGraph(sparse(I, J, V, n, n))
 
-    GeodesicComplex(pts, landmarks, graph, kdt, cover, T(r), metric, witness)
+    GeodesicComplex(pts, landmark_idxs, graph, kdt, cover, T(r), metric)
 end
 
-function getcover(P, r, kdt = KDTree(P))
-    covered = falses(length(P))
-    idxs = shuffle(eachindex(P))
+function getcover(::Nothing, pts, r, tree)
+    covered = falses(length(pts))
+    idxs = shuffle(eachindex(pts))
     landmarks = Int[]
     cover = Set{Int}[]
 
     for i in idxs
         covered[i] && continue
         covered[i] = true
-        # names!
-        inball = inrange(kdt, P[i], r)
+        inball = inrange(tree, pts[i], r)
         covered[inball] .= true
         push!(landmarks, i)
         push!(cover, Set(inball))
     end
-    landmarks, cover
+    ord = sortperm(landmarks)
+    landmarks[ord], cover[ord]
 end
 
+function getcover(landmarks, pts, r, tree)
+    cover = Set{Int}[]
+
+    for l in landmarks
+        inball = inrange(tree, pts[l], r)
+        push!(cover, Set(inball))
+    end
+    ord = sortperm(landmarks)
+    landmarks[ord], cover[ord]
+end
+
+Base.eltype(::GeodesicComplex{<:Any, P}) where {P} = P
+
 # LightGraphs interface
-for f in [:edges, :eltype, :ne, :nv, :vertices, :is_directed, :weights]
+for f in [:edges, :ne, :nv, :vertices, :is_directed, :weights]
     @eval LightGraphs.$f(gc::GeodesicComplex) = $f(gc.graph)
 end
 LightGraphs.is_directed(::Type{GeodesicComplex}) = false
-LightGraphs.is_directed(::Type{GeodesicComplex{T,D,P,M,K}}) where {T,D,P,M,K} = false
+LightGraphs.is_directed(::Type{GeodesicComplex{T,P,M,K}}) where {T,P,M,K} = false
 LightGraphs.has_edge(gc::GeodesicComplex, u::Integer, v::Integer) = has_edge(gc.graph, u, v)
 LightGraphs.has_vertex(gc::GeodesicComplex, v::Integer) = has_edge(gc.graph, v)
 LightGraphs.inneighbors(gc::GeodesicComplex, v::Integer) = inneighbors(gc.graph, v)
 LightGraphs.outneighbors(gc::GeodesicComplex, v::Integer) = outneighbors(gc.graph, v)
 
-function Base.show(io::IO, gc::GeodesicComplex{T, D}) where {T, D}
-    print(io, "GeodesicComplex{$T, $D} with $(npoints(gc)) points, " *
+function Base.show(io::IO, gc::GeodesicComplex{T, P}) where {T, P}
+    print(io, "GeodesicComplex{$T, $P} with $(npoints(gc)) points, " *
           "$(nlandmarks(gc)) landmarks and $(ne(gc)) edges")
 end
 
 npoints(gc::GeodesicComplex) =
     length(gc.points)
+
 nlandmarks(gc::GeodesicComplex) =
-    length(gc.landmarks)
-points(gc::GeodesicComplex, idxs=1:npoints(gc)) =
+    length(gc.landmark_idxs)
+
+points(gc::GeodesicComplex) =
+    gc.points
+points(gc::GeodesicComplex, idxs) =
     gc.points[idxs]
+
 landmarks(gc::GeodesicComplex, idxs=1:nlandmarks(gc)) =
-    gc.points[gc.landmarks[idxs]]
+    gc.points[gc.landmark_idxs[idxs]]
+
+landmark_idxs(gc::GeodesicComplex) =
+    gc.landmark_idxs
+landmark_idxs(gc::GeodesicComplex, idxs) =
+    gc.landmark_idxs[idxs]
+
 nonlandmarks(gc::GeodesicComplex, idxs=1:npoints(gc)-nlandmarks(gc)) =
-    gc.points[setdiff(1:npoints(gc), gc.landmarks)[idxs]]
+    gc.points[setdiff(1:npoints(gc), gc.landmark_idxs)[idxs]]
 
 # Plots recipe
 function getxyz(pts)
@@ -107,51 +125,35 @@ function getxyz(pts)
     end
 end
 
-@recipe function plot(gc::GeodesicComplex{T, D}, cycles = [];
-                      only_landmarks = true, graph = true) where {T, D}
+@recipe function plot(gc::GeodesicComplex;
+                      only_landmarks = true, graph = true)
     # edges
     if graph
         @series begin
             label := "edges"
-
-            edgepoints = SVector{D, Float64}[]
+            edgepoints = Vector{Float64}[]
             for e in edges(gc)
                 s = landmarks(gc, src(e))
                 d = landmarks(gc, dst(e))
-                append!(edgepoints, (s, d, @SVector fill(NaN, D)))
+                append!(edgepoints, (s, d, fill(NaN, length(s))))
             end
-
             getxyz(edgepoints)
         end
     end
-
     # landmarks
     @series begin
         markersize --> 1.0
         seriestype --> :scatter
         label := "landmarks"
-
         getxyz(landmarks(gc))
     end
-
     # others
     if !only_landmarks
         @series begin
             markersize --> 0.5
             seriestype --> :scatter
             label := "others"
-
-            getxyz(nonlandmarks(gc))
-        end
-    end
-
-    # cycles
-    for (i, c) in enumerate(cycles)
-        @series begin
-            label := "cycle $i"
-            linewidth := 5
-            edgepoints = vcat(landmarks(gc, c), [landmarks(gc, c[1])])
-            getxyz(edgepoints)
+            getxyz(points(gc, setdiff(1:npoints(gc), landmark_idxs(gc))))
         end
     end
 end
--- test/geodesiccomplex.jl
+++ test/geodesiccomplex.jl
@@ -2,37 +2,38 @@
 
     @testset "Construction & data structure properties" begin
         for (pts, metric, r) in datasets
-            landmarks, cover = GeodesicComplexes.getcover(pts, r, KDTree(pts, metric))
-            @test all(1 .≤ landmarks .≤ length(pts))
+            lmks, cover = GeodesicComplexes.getcover(nothing, pts, r, KDTree(pts, metric))
+            @test all(1 .≤ lmks .≤ length(pts))
             @test reduce(union, cover) == Set(1:length(pts))
-
-            for gc in (GeodesicComplex(pts, r, metric = metric),
-                       GeodesicComplex(pts, r, metric = metric, witness = false))
-
-                landmarks = landmark_points(gc)
-                witnesses = nonlandmark_points(gc)
-                @test allunique(landmarks)
-                @test allunique(witnesses)
-                @test length(intersect(landmarks, witnesses)) == 0
-                @test length(landmarks) + length(witnesses) == length(points(gc))
-                n_landmarks(gc) == length(landmarks)
-
-                @test all(weight(e) ≈ evaluate(metric, landmarks[src(e)], landmarks[dst(e)])
-                          for e in edges(gc))
-                @test all(weight(e) ≤ 2r
-                          for e in edges(gc))
+            @test issorted(lmks)
+            @test all(eachindex(lmks)) do i
+                all(evaluate(metric, pts[lmks[i]], pts[j]) ≤ r for j in cover[i])
             end
+            gc = GeodesicComplex(pts, r, metric = metric)
+
+            # TODO: indexing
+            lmrks = landmarks(gc)
+            othrs = nonlandmarks(gc)
+            @test allunique(lmrks)
+            @test allunique(othrs)
+            @test length(intersect(lmrks, othrs)) == 0
+            @test length(lmrks) + length(othrs) == length(points(gc))
+            @test Set(union(lmrks, othrs)) == Set(points(gc))
+            nlandmarks(gc) == length(lmrks)
+
+            @test all(weight(e) ≈ evaluate(metric, lmrks[src(e)], lmrks[dst(e)])
+                      for e in edges(gc))
+            @test all(r ≤ weight(e) ≤ 2r
+                      for e in edges(gc))
         end
     end
 
     @testset "LightGraphs interface" begin
         for (pts, metric, r) in datasets
-            for gc in (GeodesicComplex(pts, r, metric = metric),
-                       GeodesicComplex(pts, r, metric = metric, witness = false))
-                # Run some functions to see that the interface is defined properly
-                @test betweenness_centrality(gc) isa Array
-                @test dijkstra_shortest_paths(gc, 1) isa LightGraphs.DijkstraState
-            end
+            gc = GeodesicComplex(pts, r, metric = metric)
+            # Run some functions to see that the interface is defined properly
+            @test betweenness_centrality(gc) isa Array
+            @test dijkstra_shortest_paths(gc, 1) isa LightGraphs.DijkstraState
         end
     end
 
@@ -40,10 +41,9 @@
         # Count number of series.
         d = Dict{Symbol, Any}()
         for (pts, metric, r) in datasets
-            for gc in (GeodesicComplex(pts, r, metric = metric),
-                       GeodesicComplex(pts, r, metric = metric, witness = false))
-                @test length(RecipesBase.apply_recipe(d, gc)) == 2
-            end
+            gc = GeodesicComplex(pts, r, metric = metric)
+            # ¯\_(ツ)_/¯
+            @test length(RecipesBase.apply_recipe(d, gc)) == 2
         end
     end
 end
